2cb62b4d131a72da16895297dc357d5453fbf6e6
Torie <victoriaevandijk@gmail.com>
2016-08-31 00:28:22 +0000

    Roadmaps and Checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,6 +22,16 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
   
+  def get_roadmap(roadmap_id)
+    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)
+  end
+  
   private
    def api_url(endpoint)
      "https://www.bloc.io/api/v1/#{endpoint}"
