ba72b0dfa2fff438c97a18ac27f3c8f34cb78f33
Torie <victoriaevandijk@gmail.com>
2016-09-01 00:23:43 +0000

    Messages edited

--- lib/kele.rb
+++ lib/kele.rb
@@ -38,7 +38,7 @@ class Kele
   end
   
   def create_message(recipient_id, subject, message)
-    response = self.class.post(base_api_endpoint("messages"), body: { "user_id": id, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
+    response = self.class.post(api_url("messages"), body: { "user_id": id, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
     puts response
   end
   
